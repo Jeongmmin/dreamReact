0cb484468bd68f69917d29c9496956aee7366a1f
Jeongmmin <seo88kr@hanmail.net>
2022-02-02 20:31:10 +0900

    habbit tracker

---
+++ jm-habit-tracker/src/app.jsx
@@ -0,0 +1,8 @@
+import Habit from './components/habit';
+import './app.css';
+
+function App() {
+  return <Habit/>
+}
+
+export default App;
---
+++ wanted-clone/src/components/slide.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class slide extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default slide;
