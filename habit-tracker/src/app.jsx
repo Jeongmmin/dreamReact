8e2e425b13521adacb2f72dc519c4cd94dfa38fa
Jeongmmin <seo88kr@hanmail.net>
2022-06-02 17:35:40 +0900

    🔥 Remove file

--- habit-tracker/src/app.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './app.css';
-import Habit from './components/habit';
-
-function App() {
-  return <Habit />;
-}
-export default App;
--- jm-habit-tracker/src/app.jsx
+++
@@ -1,8 +0,0 @@
-import Habit from './components/habit';
-import './app.css';
-
-function App() {
-  return <Habit/>
-}
-
-export default App;
